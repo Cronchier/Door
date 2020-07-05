9d0d6d504bd62449dc81a366fd312be7c66579de
Cronchier <67589269+Cronchier@users.noreply.github.com>
2020-07-05 16:41:47 +0200

    100 Door test

--- src/100doors.kt
+++ src/100doors.kt
@@ -1,14 +1,16 @@
 
 
-fun oneHundredDoors(): List<Int> {
-    val doors = BooleanArray(100, { false })
-    for (i in 0..99) {
-        for (j in i..99 step (i + 1)) {
-            doors[j] = !doors[j]
+fun main () {
+    fun oneHundredDoors(): List<Int> {
+        val doors = BooleanArray(100, { false })
+        for (i in 0..99) {
+            for (j in i..99 step (i + 1)) {
+                doors[j] = !doors[j]
+            }
         }
+        return doors
+            .mapIndexed { i, b -> i to b }
+            .filter { it.second }
+            .map { it.first + 1 }
     }
-    return doors
-        .mapIndexed { i, b -> i to b }
-        .filter { it.second }
-        .map { it.first + 1 }
 }
